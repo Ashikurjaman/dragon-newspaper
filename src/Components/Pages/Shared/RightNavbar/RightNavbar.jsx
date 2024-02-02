6e331e58744345570bc3c219d2306d011945f72b
Ashikurjaman <ashikbur007@gmail.com>
2024-02-03 01:45:04 +0600

    Right navbar complete

--- src/Components/Pages/Shared/RightNavbar/RightNavbar.jsx
+++ src/Components/Pages/Shared/RightNavbar/RightNavbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import { FaGoogle,FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa6";
+import img1 from '../../../assets/qZone1.png'
+import img2 from '../../../assets/qZone2.png'
+import img3 from '../../../assets/qZone3.png'
 const RightNavbar = () => {
-    return (
-        <div>
-            <h3 className="text-2xl">Right Navbar</h3>
+  return (
+    <div>
+      <div className="pl-5 mb-5">
+        <h3 className="text-2xl mb-5 font-bold">Login With</h3>
+        <div className="mx-auto">
+        <button className="btn btn-outline btn-primary mb-3">
+            <FaGoogle/>
+            Login with Google</button>
+        <button className="btn btn-outline btn-primary">
+            <FaGithub/>
+            Login with Github</button>
         </div>
-    );
+      </div>
+        {/* second part */}
+      <div className="pl-5 mb-5">
+        <h3 className="text-2xl mb-5 font-bold">Find us on</h3>
+        <div className="mx-auto">
+        <a className="btn border rounded-t-lg w-full  ">
+            <FaFacebook/>
+            Facebook</a>
+        <a className="btn border w-full ">
+            <FaTwitter/>
+            Twitter</a>
+        <a className="btn border  w-full ">
+            <FaInstagram/>
+            Instagram</a>
+        </div>
+      </div>
+      {/* 3rd part  */}
+
+      <div className="pl-5 mb-5">
+        <h3 className="text-2xl mb-5 font-bold">Q-Zone</h3>
+        <div className="mx-auto">
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightNavbar;
--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -2,6 +2,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../Layout/Root';
 import Home from '../Pages/Home/Home';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+import Career from '../Pages/Career/Career';
+import About from '../Pages/About/About';
 
 const Routes = createBrowserRouter([
     {
@@ -11,6 +15,22 @@ const Routes = createBrowserRouter([
             {
                 path:'/',
                 element:<Home/>
+            },
+            {
+                path:'/about',
+                element:<About/>
+            },
+            {
+                path:'/career',
+                element:<Career/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
             },
         ]
       },
