2d76cf38566f04a4337e9efb8414787ebbb05203
unknown <ashikbur007@gmail.com>
2024-02-03 19:56:26 +0600

    k

--- src/Components/Context/AuthContextProvider.jsx
+++ src/Components/Context/AuthContextProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 import app from "../../Firebase/Firebae.config";
+import { useEffect } from "react";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
@@ -11,6 +12,15 @@ const AuthContextProvider = ({children}) => {
     const createUser =(email,password)=>{
       return createUserWithEmailAndPassword(auth, email, password)
     }
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            console.log('User found',currentUser);
+            setUser(currentUser);
+        })
+        return ()=>{
+            unSubscribe()
+        };
+    },[])
 
     const authInfo = {
         user,
